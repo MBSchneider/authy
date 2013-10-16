df9ef61bcf34d3cee1a5ec4fef302ca5ef4520af
mattschneider2002 <matts3@u.washington.edu>
2013-10-16 15:32:03 -0700

    Simple user authentication app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authy::Application.config.secret_token = 'e94cbe7aac75fd4fed6c937cfcc21aec5f3bb06862fbe32d7437c4256ac357df7101046c4ffef45810b2513121316818764eceb92ec85a260c8b208e173fcc93'
